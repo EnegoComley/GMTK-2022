f8e3ec45ed266f6f6ea30d378ed2bed0407485de
Neg99 <enegocomley@gmail.com>
2022-07-16 12:36:30 +0100

    Done player movement

---
+++ GMTK 2022/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private NavMeshAgent agent;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            var target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            target.z = 0;
+            agent.destination = target;
+        }
+    }
+}
