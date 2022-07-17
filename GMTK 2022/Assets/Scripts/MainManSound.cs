ca64768633c64513df905198a66f91b3a20212a0
YavorAlexandrov960 <1Yavor1@gmail.com>
2022-07-17 14:04:03 +0100

    Sound

---
+++ GMTK 2022/Assets/Scripts/MainManSound.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainManSound : MonoBehaviour
+{
+    public static MainManSound manager;
+    public AudioSource source;
+    public AudioClip[] clips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void PlaySoundNumber(int number)
+    {
+        source.PlayOneShot(clips[number]);
+    }
+}
